cde71faef12297b86d71e813b6ba64e03ddbdece
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-01-13 23:29:25 +0200

    Get a user by id

--- app/controllers/api/v1/admin/users_controller.rb
+++ app/controllers/api/v1/admin/users_controller.rb
@@ -7,6 +7,12 @@ module Api
 
           render json: @users
         end
+
+        def show
+          @user = User.find params[:id]
+
+          render json: @user
+        end
       end
     end
   end
--- spec/requests/api/v1/admin/users_spec.rb
+++ spec/requests/api/v1/admin/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/admin/users' do
   describe 'GET /users' do
-    it 'returns a list of user' do
+    it 'returns a list of users' do
       password = 'password'
       dudette = User.create email: 'dudette@example.com', password: password
       dude = User.create email: 'dude@example.com', password: password
@@ -12,8 +12,31 @@ RSpec.describe '/api/v1/admin/users' do
       expect(response).to have_http_status :ok
 
       expect(body.count).to be 2
-      expect(body.first['email']).to eq dudette.email
-      expect(body.last['email']).to eq dude.email
+      expect(body).to eq [json_for(dudette), json_for(dude)]
     end
+  end
+
+  describe 'GET /users/:id' do
+    it 'returns the specific user' do
+      dudette = User.create email: 'dudette@example.com', password: 'password'
+
+      get "/api/v1/admin/users/#{dudette.id}"
+
+      expect(body).to eq json_for(dudette)
+    end
+  end
+
+  def json_for(user)
+    {
+      'id' => user.id,
+      'provider' => 'email',
+      'uid' => user.email,
+      'name' => nil,
+      'nickname' => nil,
+      'image' => nil,
+      'email' => user.email,
+      'created_at' => user.created_at.as_json,
+      'updated_at' => user.updated_at.as_json
+    }
   end
 end
