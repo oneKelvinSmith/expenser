9e655d4f9b0f2bdeeface13d2a217e431f3f8e34
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-01-16 19:56:52 +0200

    Refactor ember login controller

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -9,14 +9,10 @@ export default Ember.Controller.extend({
     login: function authenticate() {
       const { email, password } = this.getProperties('email', 'password');
 
-      const failure = ({ error_description }) => {
-        this.set('errors', [error_description]);
-      };
-
       this
         .get('session')
         .authenticate('authenticator:oauth2', email, password)
-        .catch(failure);
+        .catch(({ error_description }) => this.set('errors', [error_description]));
     }
   }
 });
