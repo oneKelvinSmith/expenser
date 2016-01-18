91a790923c278a55a4c35318905c0cf2142b95a8
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-01-18 22:24:09 +0200

    Remove rediculous currency formatter code

--- frontend/app/helpers/format-currency.js
+++ frontend/app/helpers/format-currency.js
@@ -1,13 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(params => {
-  const value = params[0] * 100;
-  const dollars = Math.floor(value / 100);
-  let cents = value % 100;
+  const value = String(params[0]);
   const sign = '$';
 
-  if (cents.toString().length === 1) {
-    cents = '0' + cents;
-  }
-  return `${sign} ${dollars}.${cents}`;
+  return `${sign} ${value}`;
 });
